c1967c7a4e2c08dc0750d5282758a828361daa8f
Celyn Johns <143140229+CelynvJohns@users.noreply.github.com>
2023-12-05 11:43:18 -0600

    Added hashtables and linked lists

--- src/main/java/music/controller/WebController.java
+++ src/main/java/music/controller/WebController.java
@@ -3,159 +3,87 @@ package music.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
-import music.model.*;
-import music.repository.*;
+import music.model.Playlist;
+import music.model.Song;
+import music.service.MusicLibraryService;
 
 @Controller
 public class WebController {
-	// Song Repository
-	@Autowired
-	SongsRepository repo;
-	
-	//Playlist Repository
-	@Autowired
-	PlaylistRepository play;
-	
-	// Start of Song Mapping
 
-	// allows you to view all the songs and if there are none redirects you to add a song
-	/**
-	 * @param model
-	 * @return add song or song results
-	 */
-	@GetMapping({ "viewAllSongs" })
-	public String viewAllSongs(Model model) {
-		if (repo.findAll().isEmpty()) {
-			return addNewSongs(model);
-		}
+    @Autowired
+    private MusicLibraryService musicLibraryService;
 
-		model.addAttribute("Songs", repo.findAll());
-		return "songResults";
-	}
+    // Mapping for viewing all songs
+    @GetMapping("/viewAllSongs")
+    public String viewAllSongs(Model model) {
+        model.addAttribute("songs", musicLibraryService.getAllSongs());
+        return "songResults";
+    }
 
-	// allows you to input a song
-	/**
-	 * @param model
-	 * @return song input
-	 */
-	@GetMapping("/inputSongs")
-	public String addNewSongs(Model model) {
-	    Song s = new Song();
-	    model.addAttribute("newSongs", s);
-	    return "songsInput";
-	}
+    // Mapping for adding a new song
+    @GetMapping("/inputSongs")
+    public String addNewSongs(Model model) {
+        Song song = new Song();
+        model.addAttribute("newSongs", song);
+        return "songsInput";
+    }
 
-	// allows to got to the editing area
-	/**
-	 * @param id
-	 * @param model
-	 * @return song input
-	 */
-	@GetMapping("/edit/{id}")
-	public String showUpdateSongs(@PathVariable("id") long id, Model model) {
-		Song s = repo.findById(id).orElse(null);
-		System.out.println("ITEM TO EDIT: " + s.toString());
-		model.addAttribute("newSongs", s);
-		return "songsInput";
-	}
+    // Mapping for editing a song
+    @GetMapping("/edit/{id}")
+    public String showUpdateSongs(@PathVariable("id") long id, Model model) {
+        Song song = musicLibraryService.getSongById(id);
+        model.addAttribute("newSongs", song);
+        return "songsInput";
+    }
 
-	// updates song
-	/**
-	 * @param r
-	 * @param model
-	 * @return song input
-	 */
-	@PostMapping("/update/{id}")
-	public String reviseSongs(Song s, Model model) {
-		repo.save(s);
-		return viewAllSongs(model);
-	}
+    @PostMapping("/update/{id}")
+    public String reviseSongs(@ModelAttribute("newSongs") Song song, Model model) {
+        musicLibraryService.updateSong(song);
+        return "redirect:/viewAllSongs";
+    }
 
-	// deletes the song if wanted
-	/**
-	 * @param id
-	 * @param model
-	 * @return view all songs
-	 */
-	@GetMapping("/delete/{id}")
-	public String deleteSong(@PathVariable("id") long id, Model model) {
-		Song s = repo.findById(id).orElse(null);
-		repo.delete(s);
-		return viewAllSongs(model);
-	}
-	
-	// Start of Playlist Mapping
-	// allows you to view all the Playlists and if there are none redirects you to add a Playlist
-		/**
-		 * @param model
-		 * @return add Playlist or Playlist results
-		 */
-		@GetMapping({ "viewAllPlaylists" })
-		public String viewAllPlaylists(Model model) {
-			if (play.findAll().isEmpty()) {
-				return addNewPlaylists(model);
-			}
+    @GetMapping("/delete/{id}")
+    public String deleteSong(@PathVariable("id") long id, Model model) {
+        musicLibraryService.deleteSong(id);
+        return "redirect:/viewAllSongs";
+    }
 
-			model.addAttribute("Playlists", play.findAll());
-			return "playlistResults";
-		}
+    // Mapping for viewing all playlists
+    @GetMapping("/viewAllPlaylists")
+    public String viewAllPlaylists(Model model) {
+        model.addAttribute("playlists", musicLibraryService.getAllPlaylists());
+        return "playlistResults";
+    }
 
-		// allows you to input a Playlist
-		/**
-		 * @param model
-		 * @return Playlist input
-		 */
-		@GetMapping("/inputPlaylists")
-		public String addNewPlaylists(Model model) {
-		    Playlist s = new Playlist();
-		    model.addAttribute("newPlaylists", s);
-		    return "playlistInput";
-		}
+    // Mapping for adding a new playlist
+    @GetMapping("/inputPlaylists")
+    public String addNewPlaylists(Model model) {
+        Playlist playlist = new Playlist();
+        model.addAttribute("newPlaylists", playlist);
+        return "playlistInput";
+    }
 
+    // Mapping for editing a playlist
+    @GetMapping("/editPlaylist/{id}")
+    public String showUpdatePlaylists(@PathVariable("id") long id, Model model) {
+        Playlist playlist = musicLibraryService.getPlaylistById(id);
+        model.addAttribute("newPlaylists", playlist);
+        return "playlistInput";
+    }
 
-		// allows to got to the editing area
-		/**
-		 * @param id
-		 * @param model
-		 * @return Playlist input
-		 */
-		@GetMapping("/editPlaylist/{id}")
-		public String showUpdatePlaylists(@PathVariable("id") long id, Model model) {
-			Playlist p = play.findById(id).orElse(null);
-			System.out.println("ITEM TO EDIT: " + p.toString());
-			model.addAttribute("newPlaylists", p);
-			return "playlistInput";
-		}
+    @PostMapping("/updatePlaylist/{id}")
+    public String revisePlaylists(@ModelAttribute("newPlaylists") Playlist playlist, Model model) {
+        musicLibraryService.updatePlaylist(playlist);
+        return "redirect:/viewAllPlaylists";
+    }
 
-		// updates Playlist
-		/**
-		 * @param r
-		 * @param model
-		 * @return Playlist input
-		 */
-		@PostMapping("/updatePlaylist/{id}")
-		public String revisePlaylists(Playlist p, Model model) {
-		    System.out.println("Updating Playlist: " + p.toString());
-		    play.save(p);
-		    return viewAllPlaylists(model);
-		}
+    @GetMapping("/deletePlaylist/{id}")
+    public String deletePlaylist(@PathVariable("id") long id, Model model) {
+        musicLibraryService.deletePlaylist(id);
+        return "redirect:/viewAllPlaylists";
+    }
 
-
-		// deletes the Playlist if wanted
-		/**
-		 * @param id
-		 * @param model
-		 * @return view all Playlists
-		 */
-		@GetMapping("/deletePlaylists/{id}")
-		public String deletePlaylist(@PathVariable("id") long id, Model model) {
-			Playlist p = play.findById(id).orElse(null);
-			play.delete(p);
-			return viewAllPlaylists(model);
-		}
-	
+    // ... (other mappings)
 }
---
+++ src/main/java/music/service/MusicLibraryService.java
@@ -0,0 +1,70 @@
+package music.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import music.model.Playlist;
+import music.model.Song;
+import music.repository.*;
+
+import java.util.List;
+
+@Service
+public class MusicLibraryService {
+
+    @Autowired
+    private SongsRepository songRepository;
+
+    @Autowired
+    private PlaylistRepository playlistRepository;
+
+    public List<Song> getAllSongs() {
+        return songRepository.findAll();
+    }
+
+    public void addSong(Song song) {
+        songRepository.save(song);
+    }
+
+    public Song getSongById(long id) {
+        return songRepository.findById(id).orElse(null);
+    }
+
+    public void updateSong(Song song) {
+        songRepository.save(song);
+    }
+
+    public void deleteSong(long id) {
+        Song song = songRepository.findById(id).orElse(null);
+        if (song != null) {
+            songRepository.delete(song);
+        }
+    }
+
+    // More methods for managing songs...
+
+    public List<Playlist> getAllPlaylists() {
+        return playlistRepository.findAll();
+    }
+
+    public void addPlaylist(Playlist playlist) {
+        playlistRepository.save(playlist);
+    }
+
+    public Playlist getPlaylistById(long id) {
+        return playlistRepository.findById(id).orElse(null);
+    }
+
+    public void updatePlaylist(Playlist playlist) {
+        playlistRepository.save(playlist);
+    }
+
+    public void deletePlaylist(long id) {
+        Playlist playlist = playlistRepository.findById(id).orElse(null);
+        if (playlist != null) {
+            playlistRepository.delete(playlist);
+        }
+    }
+
+    // More methods for managing playlists...
+}
