eadb86ee03d2f63646677046a4bb40c5c7d6fbd9
Celyn Johns <143140229+CelynvJohns@users.noreply.github.com>
2023-12-08 15:57:55 -0600

    rest of comments

--- src/main/java/music/PlaylistMakerAndSorterApplication.java
+++ src/main/java/music/PlaylistMakerAndSorterApplication.java
@@ -1,6 +1,26 @@
 // package
 package music;
 
+/** Name : PlaylistMakerAndSorter
+ *  Created: 11/27/2023
+ *  Course: CIS 152 - Data Structure
+ *  Version: 1.0
+ *  OS: Windows 11
+ *  IDE: eclipse
+ *  Copyright : This is my own original work
+ *  based on specifications issued by our instructor
+ *  Description : An app that makes a music sorter, it allows for songs and playlists to be added, deleted, shown, and updated.
+ *  			Input: song data or playlist data
+ *  			Output: all songs, all playlists
+ *
+ *  Academic Honesty: I attest that this is my original work.
+ *  I have not used unauthorized source code, either modified or
+ *  unmodified. I have not given other fellow student(s) access
+ *  to my program.
+ *  
+ */
+
+
 // imports
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
